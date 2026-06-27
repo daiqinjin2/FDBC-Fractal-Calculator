5298e6d42d56954e4b88aec57d9f8e633034d6cc
Test <test@example.com>
2026-06-27 20:57:42 +0800

    feat: Major upgrade to Multi-Modal Input (CSV & Image) with Adaptive Z-Scale and Lossless Arbitrary Size Support

--- FractalCalculator/Properties/AssemblyInfo.cs
+++ FractalCalculator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FractalCalculator")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
